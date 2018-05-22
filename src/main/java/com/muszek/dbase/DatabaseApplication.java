fa87de9475cc0df865dafe06a4b000d82dc7a3f7
Jakub <rroott33@gmail.com>
2018-05-22 16:15:47 +0200

    sda

--- src/main/java/com/muszek/dbase/DatabaseApplication.java
+++ src/main/java/com/muszek/dbase/DatabaseApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 @SpringBootApplication
-public class DatabaseApplication {
+public class sadasdDatabaseApplication {
 
     public static void main(String[] args) {
         SpringApplication.run(DatabaseApplication.class, args);
